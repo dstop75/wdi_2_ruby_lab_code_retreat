db97045c6187ee3c1d90bafbecb77dccea2b5a10
Dan Stoppelman <dstop75@gmail.com>
2015-01-20 15:38:01 -0500

    2 tests created and passed

---
+++ lib/conway.rb
@@ -0,0 +1,12 @@
+class Cell
+  def initialize(x, y, status)
+    @x = x
+    @y = y
+    @status = status
+  end
+end
+class Grid
+  def initialize
+  end
+end
+
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,1 +1,31 @@
 require_relative '../lib/conway'
+
+# steps - intialize by randomly assigning live or dead state
+# class of cell with attributes of coordinates on the grid, and the attribute of alive or dead
+# How do we get grid coordinates? On an infinite grid? Assume a relative zero?
+
+describe Cell do
+
+ let(:status) {'live'}
+ let(:x) {2}
+ let(:y) {4}
+
+ subject(:cell) { Cell.new(status, x, y) }
+
+ describe 'initialize' do
+   it 'intializes a class Cell with an attribute status, and x,y coordinates' do
+     expect(cell).to be_a Cell
+   end
+ end
+end
+
+describe Grid do
+
+  subject(:grid) { Grid.new }
+
+  describe '#initialize' do
+    it 'creates a new instance of a Grid with x and y coordinates' do
+      expect(grid).to be_a Grid
+    end
+  end
+end
